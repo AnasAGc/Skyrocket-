b9d575161971aed52dbbf8d6604169a2fad3d901
AnasAGc <civil.anas1000@gmail.com>
2021-04-04 16:20:18 +0300

    we add some js

---
+++ Javascript/app.js
@@ -0,0 +1,12 @@
+// welcome to my humble code 
+
+let x = prompt ("Do you want to take advantage of your time","yessssss");
+let y = prompt  ("please Enter your Name  ");
+let z = prompt ("pleas Enter Fathers Name ");
+let t = prompt ("pleas Enter last Name  ");
+alert ("Hello"+" "+y+" " +z+" " +t);
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
+console.log(typeof t);
+
